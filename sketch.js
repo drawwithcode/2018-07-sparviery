3863885ad2354c62ab668b9c0532a31ea6352ddf
sparviery <37146800+sparviery@users.noreply.github.com>
2018-11-07 02:35:51 +0100

    we

--- sketch.js
+++ sketch.js
@@ -1,11 +1,49 @@
-function preload(){
-  // put preload code here
-}
+var mic;
+var capture;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+angleMode(DEGREES);
+frameRate(10);
+
+
+  // Create an Audio input
+  mic = new p5.AudioIn();
+
+  // start the Audio Input.
+  mic.start();
+
+  capture = createCapture(VIDEO);
+    capture.size(640, 480);
+    capture.hide();
 }
 
 function draw() {
-  // put drawing code here
+
+  background(220);
+
+  //
+  var vol = mic.getLevel();
+
+  push();
+  translate(width/2, height/2);
+  rotate((frameCount)*(vol*1000));
+  var r=200;
+  for(var a=0; a<360; a += 60){
+
+    var x = r*cos(a);
+    var y = r*sin(a);
+    imageMode(CENTER);
+    noStroke();
+    var myImage = capture.loadPixels();
+    image(myImage, x, y, 64+(vol*640), 48+(vol*480));
+}
+  pop();
+
+push();
+      translate(width/2, height/2);
+      textSize(20);
+      textAlign(CENTER);
+      text('SHOUT TO SPIN', 0 , 0);
+pop();
 }
